a9aaf8baa9417ada71603254a0f03a50e4baa976
Buchanonxd <kiborajames88@gmail.com>
2023-10-05 05:28:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - My program on printing with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
