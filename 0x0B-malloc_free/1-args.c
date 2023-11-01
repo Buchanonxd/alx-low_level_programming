fb7bcb8b3149f6f24f474313b5f47bb70443e44f
Buchanonxd <kiborajames88@gmail.com>
2023-11-01 12:06:17 -0700

    1-args.c

---
+++ 0x0B-malloc_free/1-args.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc:the argument count
+ * @argv:the argument value
+ * Return: 0 if successful
+*/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	int i;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		for (i = 0; i <= argc; i++)
+		{
+			;
+		}
+
+
+	printf("%d\n", argc - 1);
+	}
+
+	return (0);
+}
