263eab65c000ff3dd7cf4c45896bcd8fd7d4e13e
Buchanonxd <kiborajames88@gmail.com>
2024-01-11 01:47:36 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print a sting in reverse
+ * @s: string to reverse
+ * Rerutn: is void
+*/
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
