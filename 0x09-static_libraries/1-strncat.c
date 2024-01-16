eaa16e687e187fcd95f75d487a1598e73d8d326f
Buchanonxd <kiborajames88@gmail.com>
2024-01-16 01:23:36 -0800

    static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: the origin
+ * @n: the bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while (dest[a])
+		a++;
+
+	for (b = 0; b < n && src[b]; a++, b++)
+		dest[a] = src[b];
+	return (dest);
+}
